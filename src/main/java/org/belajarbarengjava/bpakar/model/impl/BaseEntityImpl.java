d6454f7190629b7950fe5adff80545e8bc39bf5d
Ferdinand Neman <ferdinand.neman@gmail.com>
2015-03-12 13:46:11 +0800

    Added proper hibernate dependencies into POM. added annotation to the base entity.

--- src/main/java/org/belajarbarengjava/bpakar/model/impl/BaseEntityImpl.java
+++ src/main/java/org/belajarbarengjava/bpakar/model/impl/BaseEntityImpl.java
@@ -1,12 +1,24 @@
 package org.belajarbarengjava.bpakar.model.impl;
 
 import org.belajarbarengjava.bpakar.model.spec.BaseEntity;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.annotation.Generated;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 
 /**
  * Created by ferdinand on 3/12/15.
  */
+@MappedSuperclass
 public class BaseEntityImpl implements BaseEntity {
 
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    @Column(name = "theid", unique = true)
     private String id;
 
     @Override
